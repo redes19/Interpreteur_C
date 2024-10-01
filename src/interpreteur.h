093e37cda8c6badbf7929ebfd8b3f1441f6e9bbd
Loris Laurenti <lloris19@outlook.fr>
2024-10-01 10:44:28 +0200

    add structure

--- src/interpreteur.h
+++ src/interpreteur.h
@@ -1,6 +1,25 @@
 #ifndef INTERPRETEUR_H
 #define INTERPRETEUR_H
 
+typedef enum {
+    NUMBER,
+    PLUS,
+    MINUS,
+    MULT,
+    DIV,
+    MOD,
+    POW,
+    LPAREN,
+    RPAREN,
+    TOEKN_EOF,
+} tokenType;
+
+typedef struct {
+    tokenType type;
+    int value;
+} token;
+
+
 void interpreteur();
 
 #endif
