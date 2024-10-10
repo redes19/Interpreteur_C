b14cd05414832d1c63c00c2f0467d87871965418
Loris Laurenti <lloris19@outlook.fr>
2024-10-10 16:53:35 +0200

    prepare code

--- src/main.c
+++ src/main.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 #include "lexer.h"
 #include <stdlib.h>
-#include "parser.h"
-#include "ast.h"
 
  int main()
 {
      const char *input = "3 + 4 * 8";
      Token *tokens = lexer(input);
 
-     Parser parser = init_parser(tokens);
-     ASTNode *ast = parse_expression(&parser);
-
-     int result = eval(ast);
-     printf("Résultat : %d\n", result);
 
      free(tokens);
     return 0;
