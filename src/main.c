349a68c908026aacdb135b1df357db3b490cfaba
Loris Laurenti <lloris19@outlook.fr>
2024-11-04 15:50:40 +0100

    last main valid

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 #include "lexer.h"
 #include "parser.h"
@@ -54,4 +54,34 @@ int main(int argc, char *argv[]) {
     if (argc >= 2) free((void*)input);
 
     return 0;
-}
+}*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "lexer.h"
+#include "AST.h"
+
+ int main()
+ {
+     const char *input = "3 + 4 * 2 / (5 - 2)";
+     //const char *input = "xax = 3 + 3 * (2 + 1)";
+     //const char *input = "a = (10 + 2) * 3";
+
+     Token *tokens = lexer(input);
+
+     ASTNode *ast = parser_ast(tokens);
+     if (ast == NULL) {
+         printf("Erreur: AST est NULL\n");
+         exit(1);
+     }
+     printf("\nretour node\n");
+     print_ast(ast);
+
+
+     const int result = eval_ast(ast);
+     printf("\nResult : %d\n", result);
+
+     free_ast(ast);
+     free(tokens);
+     return 0;
+ }
