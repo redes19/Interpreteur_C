5ede17c66352fdae8f37154cf7656aa96fc19f76
Loris Laurenti <lloris19@outlook.fr>
2024-11-17 21:00:01 +0100

    correctif

--- src/lexer.c
+++ src/lexer.c
@@ -73,11 +73,6 @@ Token *lexer(const char *input)
     while (input[position] != '\0') {
         char pos_char = input[position];
 
-        if (pos_char == '\0') {
-            position++;
-            continue;
-        }
-
         if(pos_char == ' ') {
             position++;
             continue;
